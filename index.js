49939640c6354448556d41cda3752220b93e4d90
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2021-09-13 04:28:02 +1200

    made some progress. All API calls were successful

---
+++ index.js
@@ -0,0 +1,103 @@
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=London&APPID=0b104378f4050528fb54062500335403
+
+// const h1 = document.getElementById("h1");
+// h1.textContent = "sldjflajfsd";
+
+
+
+function displayHome() {
+    const content = document.getElementById("h1");
+    console.log(document)
+    console.log(content);
+    content.innerHTML = ""
+    // content.appendChild(mainPage());
+}
+
+// const getdetails = async function(query) {
+//     // const img = document.querySelector('img');
+//     const weatherJson = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${query}&APPID=0b104378f4050528fb54062500335403`, {mode: 'cors'})
+//     console.log(weatherJson);
+//     const weather = await weatherJson.json()
+//     console.log(weather);
+//     console.log(weather.weather[0].description)
+//     console.log(weather.main)
+//     console.log(weather.main.temp)
+//     console.log(weather.main.temp_min)
+//     console.log(weather.main.temp_max)
+//     console.log(weather.sys.sunset)
+//     // const change = await 
+// }
+
+
+// getdetails('london')
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        query: 'Christchurch',
+        location: '',
+        locationImg: '',
+        description: '',
+        descriptionImg: '',
+        temp: '',
+        temp_min: '',
+        temp_max: '',
+        sunset: '',
+        error: ''
+
+
+    },
+
+
+    methods: {
+         async getdetails() {
+            try {
+                const weatherJson = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${this.query}&APPID=0b104378f4050528fb54062500335403`, {mode: 'cors'})
+                console.log(weatherJson);
+                const weather = await weatherJson.json()
+                console.log(weather);
+                this.location = weather.name
+                this.description = weather.weather[0].description;
+                
+                this.temp = weather.main.temp;
+                this.temp_min = weather.main.temp_min
+                this.temp_max = weather.main.temp_max
+                this.sunset = weather.sys.sunset
+                this.setImages()
+                this.reset()
+            } catch (error) {
+                this.error = error
+            }
+        },
+        reset : function() {
+            this.query = ''
+            this.error = ''
+        },
+        async setImages() {
+            try {
+                const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=541OEcCxCuoK1F80mZD59fBgHN8ppeZn&s=${this.location}`, {mode: 'cors'})
+                const imgJson = await response.json();
+                this.locationImg = await imgJson.data.images.original.url;
+            } catch (error) {
+                this.locationImg = 'https://cdn.mos.cms.futurecdn.net/yCPyoZDQBBcXikqxkeW2jJ-1200-80.jpg'
+            }
+
+            try {
+                const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=541OEcCxCuoK1F80mZD59fBgHN8ppeZn&s=${this.description}`, {mode: 'cors'})
+                const imgJson = await response.json();
+                this.descriptionImg = await imgJson.data.images.original.url;
+            } catch (error) {
+                this.locationImg = 'https://cdn.mos.cms.futurecdn.net/yCPyoZDQBBcXikqxkeW2jJ-1200-80.jpg'
+            }
+            console.log(this.locationImg)
+            
+            
+        }
+    },
+    beforeMount(){
+        this.getdetails()
+     },
+    
+  })
